743b5989d87df33f355666aeff7467cbb9136ab2
Tom Garcia <tomgarcia85@gmail.com>
2019-05-13 17:38:14 -0500

    Add functionality to forms using local reference
    - Also Add double binding

--- CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,8 +7,11 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Cookie Recipe', 'Mexican Cookie', 'https://www.loveandtacos.com/wp-content/uploads/2017/09/' +
+      'Hojarascas-Mexican-Shortbread-Cinnamon-Sugar-Cookies.jpg'),
+    new Recipe('Cookie Recipe 2', 'Mexican Cookie Again', 'https://www.loveandtacos.com/wp-content/uploads/2017/09/' +
       'Hojarascas-Mexican-Shortbread-Cinnamon-Sugar-Cookies.jpg')
   ];
 
@@ -16,6 +19,10 @@ export class RecipeListComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+      this.recipeWasSelected.emit(recipe);
   }
 
 }
